f4b8b96741c77fd0457ce6ca0b88158263a4c5d9
bbaneydev <baneyjb@gmail.com>
2024-06-29 16:44:09 -0500

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Billy Baney on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
